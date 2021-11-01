9b28271291530442b91cc0e3453000ef0d3599ac
rattanakchea <rattanak.chea@gmail.com>
2021-11-01 00:53:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mat-select-autocomplete-5aenyf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/mat-select-autocomplete-5aenyf)
---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component, ViewChild } from '@angular/core';
+import { SelectAutocompleteComponent } from 'mat-select-autocomplete';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  @ViewChild(SelectAutocompleteComponent) multiSelect: SelectAutocompleteComponent;
+  options = [
+    {
+      display: 'One',
+      value: '1'
+    }, {
+      display: 'Two',
+      value: '2'
+    }, {
+      display: 'Three',
+      value: '3'
+    }, {
+      display: 'Four',
+      value: '4'
+    }, {
+      display: 'Five',
+      value: '5'
+    }, {
+      display: 'Six',
+      value: '6'
+    }
+  ];
+  selectedOptions = ['1', '2', '3', '4'];
+
+  selected = this.selectedOptions;
+  showError = false;
+  errorMessage = '';
+
+  onToggleDropdown() {
+    this.multiSelect.toggleDropdown();
+  }
+
+  getSelectedOptions(selected) {
+    this.selected = selected;
+  }
+
+  onResetSelection() {
+    this.selectedOptions = [];
+  }
+}
